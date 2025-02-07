4e643f6afcbc7b3d2857559ecd34a0883552fa48
julieoyen <juloye51070@stud.noroff.no>
2025-02-07 19:39:03 +0100

    updated circle

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -5,7 +5,7 @@ export default function LoginForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   return (
-    <div className="p-4 w-lg max-w-full md:min-w-xl">
+    <div className="p-4 w-lg max-w-full lg:min-w-xl">
       <form
         action=""
         className="w-full flex-grow  mt-4 lg:pt-11 lg:px-11"
